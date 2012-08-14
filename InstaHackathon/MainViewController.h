705425abbdacb428585c92b0b0bfe9a7d387c69e
Omar Khan <okhan80@gmail.com>
2012-08-14 14:38:50 -0400

    Initial Commit

---
+++ InstaHackathon/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  InstaHackathon
+//
+//  Created by Omar Khan on 8/14/12.
+//  Copyright (c) 2012 SPARC. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate, UIPopoverControllerDelegate>
+
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@property (strong, nonatomic) UIPopoverController *flipsidePopoverController;
+
+@end
