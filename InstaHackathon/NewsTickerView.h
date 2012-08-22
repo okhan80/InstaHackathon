cd9a75eb054d8874facc100372c9665c9bf0002e
Omar Khan <okhan80@gmail.com>
2012-08-21 23:55:04 -0400

    Class for setting up a news ticker to show categories.

---
+++ InstaHackathon/NewsTickerView.h
@@ -0,0 +1,39 @@
+//
+//  NewsTickerView.h
+//  InstaHackathon
+//
+//  Created by Omar Khan on 8/21/12.
+//  Copyright (c) 2012 SPARC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class NewsTickerView;
+
+@protocol NewsTickerViewDataSource <NSObject>
+
+@required
+- (UIColor *)backgroundColorForTickerView:(NewsTickerView *)tickerView;
+- (int)numberOfItemsForTickerView:(NewsTickerView *)tickerView;
+
+- (NSString *)tickerView:(NewsTickerView *)tickerView titleForItemAtIndex:(NSUInteger)index;
+- (NSString *)tickerView:(NewsTickerView *)tickerView valueForItemAtIndex:(NSUInteger)index;
+- (UIImage *)tickerView:(NewsTickerView *)tickerView imageForItemAtIndex:(NSUInteger)index;
+
+@end
+
+@interface NewsTickerView : UIScrollView
+
+@property (weak, nonatomic) IBOutlet id<NewsTickerViewDataSource> dataSource;
+
+- (void)reloadData;
+- (void)startAnimation;
+
+@end
+
+@interface NewsTickerItemView : UIView
+
+- (CGFloat)width;
+- (void)setTitle:(NSString *)title value:(NSString *)value image:(UIImage *)image;
+
+@end
